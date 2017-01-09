45b80bf16a3f409683a7b60802a29a44b3a70a7b
Jan Chalabala <chalabaj@403-a324-01.vscht.cz>
2017-01-09 11:41:40 +0100

    argon

--- octopus_analyzer.f90
+++ octopus_analyzer.f90
@@ -1,10 +1,7 @@
 ! compile: gfortran -o octopus_analyzer octopus_analyzer_new2.f90
 ! progam reads line format geometry and transforms it to normal XYZ file 
 ! energies are extracted and related to the first point to eV units
-! git add
-! git push
-! git commit
-! neon 
+! argon l$ll
 program octopus_analyze
   implicit none
   INTEGER, external :: Factorial
