a4155fb9be7126f6ba66e9b3eb6f481987b475a8
Jan Chalabala <chalabaj@403-a324-01.vscht.cz>
2017-01-09 11:01:50 +0100

    compile

--- octopus_analyzer_new2.f90
+++ octopus_analyzer_new2.f90
@@ -1,4 +1,4 @@
-! compile:gfortran -o octopus_analyzer octopus-bond-energy.f90
+! compile: gfortran -o octopus_analyzer octopus_analyzer_new2.f90
 ! progam reads line format geometry and transforms it to normal XYZ file 
 ! energies are extracted and related to the first point to eV units
 
