6246abdab5e15c309e881103b252f90540a08f30
Jan Chalabala <chalabaj@403-a324-01.vscht.cz>
2017-01-09 15:14:34 +0100

    origin

--- octopus_analyzer.f90
+++ octopus_analyzer.f90
@@ -1,8 +1,6 @@
 ! compile: gfortran -o octopus_analyzer octopus_analyzer_new2.f90
 ! progam reads line format geometry and transforms it to normal XYZ file 
 ! energies are extracted and related to the first point to eV units
-! neon
-!
 program octopus_analyze
   implicit none
   INTEGER, external :: Factorial
